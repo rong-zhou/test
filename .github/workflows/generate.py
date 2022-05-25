feb4fb15e4728ca27acc3f4405d906f40b68b807
minyu luo <minyu.luo.ext@tomoloyalty.com>
2022-05-25 16:07:53 +0800

    change

--- .github/workflows/generate.py
+++
@@ -1,7 +0,0 @@
-from argparse import ArgumentParser
-
-if __name__ == '__main__':
-    parser = ArgumentParser()
-    parser.add_argument("pool", type=int)
-    parser.add_argument("-ns", "--shadow", default=0, type=int)
-    args = parser.parse_args()
