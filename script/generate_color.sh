3f57c38c0a385cee7bba5332694ac09148713390
RongZhou <rong.zhou.ext@tomoloyalty.com>
2024-10-08 15:59:01 +0800

    fix output

--- script/generate_color.sh
+++ script/generate_color.sh
@@ -6,5 +6,7 @@ colors=("red" "green" "blue")
 # Convert the array to a JSON array format
 colors_json=$(printf '%s\n' "${colors[@]}" | jq -R . | jq -s .)
 
+colors_json=$(echo "$colors_json" | tr -d '[:space:]')
+
 # Output the colors as a single string
-echo "colors=$colors_json" >> "$GITHUB_OUTPUT"
+echo "matrix={\"colors\":$colors_json}" >> $GITHUB_OUTPUT
