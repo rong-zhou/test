e4820ea71a836a1eed8ddd034b982f260eb881d9
RongZhou <rong.zhou.ext@tomoloyalty.com>
2024-10-08 16:24:41 +0800

    fix output

--- script/generate_color.sh
+++ script/generate_color.sh
@@ -9,4 +9,4 @@ colors_json=$(printf '%s\n' "${colors[@]}" | jq -R . | jq -s .)
 colors_json=$(echo "$colors_json" | tr -d '[:space:]')
 
 # Output the colors as a single string
-echo "matrix={\"colors\":$colors_json}"
+echo "matrix={\"colors\":$colors_json}" >> $GITHUB_OUTPUT
