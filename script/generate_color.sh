fc845bcddc84956f136a3b197345eb93609dee27
RongZhou <rong.zhou.ext@tomoloyalty.com>
2024-10-08 15:33:42 +0800

    fix output

--- script/generate_color.sh
+++ script/generate_color.sh
@@ -1,7 +1,10 @@
-#! /bin/bash
+#!/bin/bash
 
-color=("red" "green" "blue")
-echo "generate colors"
+# Define the color array
+colors=("red" "green" "blue")
 
-# shellcheck disable=SC2128
-echo "colors=$color" >> "$GITHUB_OUTPUT"
+# Join the array into a comma-separated string
+colors_string=$(IFS=,; echo "${colors[*]}")
+
+# Output the colors as a single string
+echo "$colors_string" >> "$GITHUB_OUTPUT"
